ff9a0c91414effe8c82a0768a23fdd3637cdf1fb
Swaroop Joshi <5982730+swaroopjcse@users.noreply.github.com>
2017-10-08 22:54:21 -0400

    Encryption info added to org.el

--- org.el
+++ org.el
@@ -72,4 +72,53 @@
 (org-crypt-use-before-save-magic)
 (setq org-tags-exclude-from-inheritance (quote ("crypt")))
                                         ; GPG key to use for encryption
-(setq org-crypt-key "7B8810EF0F59C496C78F8EF15C07A55E01FCA00C")
+
+;; GPG key to use for encryption
+;; Either the Key ID or set to nil to use symmetric encryption.
+(setq org-crypt-key nil)
+;; (setq org-crypt-key "7B8810EF0F59C496C78F8EF15C07A55E01FCA00C")
+
+(setq auto-save-default nil)
+;; Auto-saving does not cooperate with org-crypt.el: so you need
+;; to turn it off if you plan to use org-crypt.el quite often.
+;; Otherwise, you'll get an (annoying) message each time you
+;; start Org.
+
+;; To turn it off only locally, you can insert this:
+;;
+;; # -*- buffer-auto-save-file-name: nil; -*-
+
+(require 'epa-file)
+(epa-file-enable)
+(setq epa-file-select-keys nil)
+;; Set GPG program
+(custom-set-variables
+ '(epg-gpg-program "/Users/swaroop/.brew/homebrew/bin/gpg2"))
+;; https://colinxy.github.io/software-installation/2016/09/24/emacs25-easypg-issue.html
+(setf epa-pinentry-mode 'loopback)`
+
+;; transparently decrypt/encrypt things
+(setq epa-armor t)
+(setq epa-file-name-regexp "\\.\\(gpg\\|asc\\)$")
+(epa-file-name-regexp-update)
+
+;; Set the following as the first line of the file to be encrypted
+;; For symmetric encryption, remove the 'epa-file-encrypt-to:' part
+;; # -*- mode:org; epa-file-encrypt-to: ("me@mydomain.com") -*-
+
+;; Minor mode for ASCII-armored gpg-encrypted files
+;; From https://unix.stackexchange.com/questions/55354/how-to-use-emacs-to-recognize-and-automatically-open-gpg-encrypted-file-in-ascii#55362
+(define-minor-mode auto-encryption-armored-mode
+  "Save files in encrypted, ASCII-armored format"
+  ;; The initial value.
+  nil
+  ;; The indicator for the mode line.
+  " Encrypted,Armored"
+  ;; The minor mode bindings.
+  nil
+  (if (symbol-value auto-encryption-armored-mode)
+      (set (make-local-variable 'epa-armor) t)
+    (kill-local-variable 'epa-armor))
+  )
+
+(add-to-list 'auto-mode-alist '("\\.asc$" . auto-encryption-armored-mode))
