51b1dedead5a831bdd91a69f5702c1bf9c474c34
Swaroop Joshi <5982730+swaroopjcse@users.noreply.github.com>
2020-06-14 00:59:23 -0600

    org-latex-classes was throwing errors; need to investigate, disabled for now

--- init.el
+++ init.el
@@ -524,17 +524,17 @@ you should place your code here."
                                           ;; sections, etc.
 
 ;; **** org-latex classes
-    (add-to-list 'org-latex-classes
-	               '("IEEEtran"
-	                 "\\documentclass{IEEEtran}
- [NO-DEFAULT-PACKAGES]
- [PACKAGES]
- [EXTRA]"
-	                 ("\\section{%s}" . "\\section*{%s}")
-	                 ("\\subsection{%s}" . "\\subsection*a{%s}")
-	                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-	                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
-	                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+    ;; (add-to-list 'org-latex-classes
+	               ;; '("IEEEtran"
+	                 ;; "\\documentclass{IEEEtran}
+ ;; [NO-DEFAULT-PACKAGES]
+ ;; [PACKAGES]
+ ;; [EXTRA]"
+	                 ;; ("\\section{%s}" . "\\section*{%s}")
+	                 ;; ("\\subsection{%s}" . "\\subsection*a{%s}")
+	                 ;; ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+	                 ;; ("\\paragraph{%s}" . "\\paragraph*{%s}")
+	                 ;; ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
 ;; *** misc
 ;; **** yankpad 
